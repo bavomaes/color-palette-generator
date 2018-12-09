801018a88f396baf8ef2d0e0d16e3f4a24072831
Bavo <b4vom4es@gmail.com>
2018-12-09 19:14:50 +0100

    Fix path + Preload image + Store all RGBA values in array

--- js/palette.js
+++ js/palette.js
@@ -3,23 +3,30 @@ let height = 317;
 let img;
 let pixelD;
 let halfImage;
+let allPixels;
+
+function preload() {
+    img = loadImage("img/monet.jpg");
+}
 
 function setup() {
     createCanvas(width, height * 2);
     background(0);
-    img = loadImage("../img/monet.jpg");
     pixelD = pixelDensity();
     halfImage = 4 * (width  * pixelD) * (height * pixelD);
+    allPixels = [];
+    noLoop();
 }
 
 function draw() {
-    image(img, 0, 0, width, height);
+    image(img, 0, 0);
     loadPixels();
     for (let i = 0; i < halfImage; i += 4) {
         pixels[i + halfImage] = pixels[i]
-        pixels[i + halfImage + 1] = pixels[i + 1]
-        pixels[i + halfImage + 2] = pixels[i + 2]
-        pixels[i + halfImage + 3] = pixels[i + 3]
+        pixels[i + halfImage + 1] = pixels[i + 1];
+        pixels[i + halfImage + 2] = pixels[i + 2];
+        pixels[i + halfImage + 3] = pixels[i + 3];
+        allPixels.push([pixels[i + halfImage], pixels[i + halfImage + 1], pixels[i + halfImage + 2], pixels[i + halfImage + 3]]);
     }
     updatePixels();
 }
